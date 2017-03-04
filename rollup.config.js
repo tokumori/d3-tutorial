c8e1195ea8c3962c96feb06dd2d14e081e00ef81
Bryce <bsaito88@gmail.com>
2017-03-04 10:22:29 -1000

    Added SASS-like partial support.

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+import postcss from 'rollup-plugin-postcss';
+import simplevars from 'postcss-simple-vars';
+import nested from 'postcss-nested';
+import cssnext from 'postcss-cssnext';
+import cssnano from 'cssnano';
+import partial from 'postcss-partial-import';
+
+export default {
+  entry: 'src/app.js',
+  dest: 'public/js/app.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  plugins: [
+    postcss({
+      plugins: [
+        simplevars(),
+        nested(),
+        cssnext({warnForDuplicates: false}),
+        partial({prefix: '_'}),
+        cssnano(),
+      ],
+      extensions: ['.css'],
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    eslint({
+      exclude: [
+        'src/styles/**',
+      ]
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    (process.env.NODE_ENV === 'production' && uglify()),
+  ],
+};
