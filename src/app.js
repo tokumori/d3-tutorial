462fe5742c4b771f090da6399a00af6ce6315bc5
Bryce <bsaito88@gmail.com>
2017-03-04 18:25:59 -1000

    Changed to svg.

--- src/app.js
+++ src/app.js
@@ -17,15 +17,32 @@ if (ENV !== 'production') {
   debug.disable();
 }
 
-var data = [4, 8, 15, 16, 23, 42];
+const data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scaleLinear()
+const width = 420,
+  barHeight = 20;
+
+const x = d3.scaleLinear()
     .domain([0, d3.max(data)])
-    .range([0, 420]);
+    .range([0, width]);
 
-d3.select('.chart')
-  .selectAll('div')
+const chart = d3.select('.chart')
+  .attr('width', width)
+  .attr('height', barHeight * data.length);
+
+const bar = chart.selectAll('g')
     .data(data)
-  .enter().append('div')
-    .style('width', function(d) { return x(d) + 'px'; })
-    .text(function(d) { return d; });
+  .enter().append('g')
+    .attr('transform', function(d, i) {
+      return `translate(0,${i * barHeight})`;
+    });
+
+bar.append('rect')
+  .attr('width', x)
+  .attr('height', barHeight - 1);
+
+bar.append('text')
+  .attr('x', function(d) { return x(d) - 3; })
+  .attr('y', barHeight / 2)
+  .attr('dy', '.35em')
+  .text(function(d) { return d; });
