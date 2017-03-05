8e993c3a957bcb8c0b0f26597dde557e500246ab
Bryce <bsaito88@gmail.com>
2017-03-05 01:02:56 -1000

    Cleaned up master.

--- src/app.js
+++ src/app.js
@@ -16,14 +16,3 @@ if (ENV !== 'production') {
 } else {
   debug.disable();
 }
-
-const svg = d3.select('svg');
-const circle = svg.selectAll("circle")
-    .data([32, 57, 293], function(d) { return d; });
-
-circle.enter().append("circle")
-    .attr("cy", 60)
-    .attr("cx", function(d, i) { return i * 100 + 30; })
-    .attr("r", function(d) { return Math.sqrt(d); });
-
-circle.exit().remove();
