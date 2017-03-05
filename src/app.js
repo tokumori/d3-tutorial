8977de64ec8b897b24866d0e07db2a808e2b321a
Bryce <bsaito88@gmail.com>
2017-03-04 18:36:31 -1000

    Modified circles' styles and attr.

--- src/app.js
+++ src/app.js
@@ -17,32 +17,7 @@ if (ENV !== 'production') {
   debug.disable();
 }
 
-const data = [4, 8, 15, 16, 23, 42];
+const circle = d3.selectAll('circle');
 
-const width = 420,
-  barHeight = 20;
-
-const x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
-
-const chart = d3.select('.chart')
-  .attr('width', width)
-  .attr('height', barHeight * data.length);
-
-const bar = chart.selectAll('g')
-    .data(data)
-  .enter().append('g')
-    .attr('transform', function(d, i) {
-      return `translate(0,${i * barHeight})`;
-    });
-
-bar.append('rect')
-  .attr('width', x)
-  .attr('height', barHeight - 1);
-
-bar.append('text')
-  .attr('x', function(d) { return x(d) - 3; })
-  .attr('y', barHeight / 2)
-  .attr('dy', '.35em')
-  .text(function(d) { return d; });
+circle.style('fill', 'steelblue')
+  .attr('r', 30);
