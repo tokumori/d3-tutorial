505b2791176d0ebc6b1b8ab4e248bc7883e0f8ad
Bryce <bsaito88@gmail.com>
2017-03-04 17:33:16 -1000

    Added debugger and livereload.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import './styles/styles.css';
 import * as d3 from './d3modules.js';
+import debug from 'debug';
+const log = debug('app:log');
+
+if (ENV !== 'production') {
+  // Enable logger.
+  debug.enable('*');
+  log('Logging is enabled!');
+
+  // Enable LiveReload
+  document.write(
+    '<script src="http://' + (location.host || 'localhost').split(':')[0] +
+    ':35729/livereload.js?snipver=1"></' + 'script>'
+  );
+} else {
+  debug.disable();
+}
 
 var data = [4, 8, 15, 16, 23, 42];
 
